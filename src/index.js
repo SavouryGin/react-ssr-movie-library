dee3bf4ab6484560e4328606729091ab655abc68
Dmitrii Suroviagin <Dmitrii_Suroviagin@epam.com>
2022-05-05 10:55:42 +0200

    install sass & add reset.scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./components/app";
 import reportWebVitals from "./reportWebVitals";
+
+// Custom styles
+import "./styles/reset.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
