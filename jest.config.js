ed0ea85afbb22240f32e44065aa8701693243efe
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-05-19 11:55:07 +0200

    task-2: add other config files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^components(.*)$': '<rootDir>/src/components$1',
+    '^__mocks__(.*)$': '<rootDir>/src/__mocks__$1',
+    '\\.(css|scss)$': '<rootDir>/src/__mocks__/style-mock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/helpers/test-utils/setup-tests.ts'],
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/app";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/app';
 
 // React 18 new root API
-const rootContainer = document.getElementById("root");
-if (!rootContainer) throw new Error("Failed to find the root element.");
+const rootContainer = document.getElementById('root');
+if (!rootContainer) throw new Error('Failed to find the root element.');
 
 const root = createRoot(rootContainer);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
