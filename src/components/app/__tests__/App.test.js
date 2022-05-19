068f1652d3ac813f648e59a608bf1df46120c621
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-05-19 10:30:38 +0200

    remove CRA template files

--- src/components/app/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../index";
-
-test("renders learn react link", () => {
-  render(<App />);
-  // eslint-disable-next-line testing-library/no-debugging-utils
-  screen.debug();
-});
