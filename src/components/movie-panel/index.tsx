1aa6a1807c24628281778c6f25568012b6223219
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-05-30 11:26:11 +0200

    task-3: add styles to the MoviePanel

--- src/components/movie-panel/index.tsx
+++ src/components/movie-panel/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { Guid } from 'guid-typescript';
 import { MoviePanelProps } from 'types/movies';
+import './styles.scss';
 
 const MoviePanel = ({ className, items }: MoviePanelProps) => {
   const panelClass = classNames('movie-panel', { [`${className}`]: !!className });
@@ -10,10 +11,21 @@ const MoviePanel = ({ className, items }: MoviePanelProps) => {
   const movies = items.map((item) => {
     const id = Guid.create().toString();
 
-    return <Movie key={id} title={item.title} year={item.year} genres={item.genres} imagePath={item.imagePath} />;
+    return (
+      <li key={id} className='movie-panel__item'>
+        <Movie title={item.title} year={item.year} genres={item.genres} imagePath={item.imagePath} />
+      </li>
+    );
   });
 
-  return <ul className={panelClass}>{movies}</ul>;
+  return (
+    <div className={panelClass}>
+      <p className='movie-panel__counter'>
+        <strong>{`${movies.length}`}</strong> movies found
+      </p>
+      <ul className='movie-panel__list'>{movies}</ul>
+    </div>
+  );
 };
 
 export default MoviePanel;
