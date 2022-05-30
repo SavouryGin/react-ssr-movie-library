77217f8f29af2385ce4b49d073a744124563d469
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-05-30 10:38:42 +0200

    task-3: add styles to the Movie component

--- src/__mocks__/movie-list.ts
+++ src/__mocks__/movie-list.ts
@@ -7,4 +7,34 @@ export const movieList: MovieItem[] = [
     year: 2004,
     imagePath: 'assets/images/Bitmap-1.jpg',
   },
+  {
+    title: 'Bohemian Rhapsody',
+    genres: ['Drama', 'Biography', 'Music'],
+    year: 2003,
+    imagePath: 'assets/images/Bitmap-2.jpg',
+  },
+  {
+    title: 'Kill Bill: Vol 2',
+    genres: ['Oscar winning Movie'],
+    year: 1994,
+    imagePath: 'assets/images/Bitmap-3.jpg',
+  },
+  {
+    title: 'Avengers: War of Infinity',
+    genres: ['Action & Adventure'],
+    year: 2004,
+    imagePath: 'assets/images/Bitmap-4.jpg',
+  },
+  {
+    title: 'Inception',
+    genres: ['Action & Adventure'],
+    year: 2003,
+    imagePath: 'assets/images/Bitmap-5.jpg',
+  },
+  {
+    title: 'Reservoir dogs',
+    genres: ['Oscar winning Movie'],
+    year: 1994,
+    imagePath: 'assets/images/Bitmap-6.jpg',
+  },
 ];
