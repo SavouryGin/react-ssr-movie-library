175f04ca8ef21f9afc92d0f27846fe2e9af38913
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-06-11 18:05:54 +0200

    task-5: add mock descriptions

--- src/__mocks__/movie-list.ts
+++ src/__mocks__/movie-list.ts
@@ -45,7 +45,8 @@ export const movieList: MovieItem[] = [
     url: '',
     rating: 7.4,
     runtime: 135,
-    overview: '',
+    overview:
+      'The Bride continues her quest of vengeance against her former boss and lover Bill, the reclusive bouncer Budd, and the treacherous, one-eyed Elle.',
   },
   {
     id: 'test-movie-3',
@@ -59,7 +60,7 @@ export const movieList: MovieItem[] = [
     url: '',
     rating: 8.2,
     runtime: 125,
-    overview: '',
+    overview: 'Avengers: Infinity War is a 2018 American superhero film based on the Marvel Comics superhero team the Avengers.',
   },
   {
     id: 'test-movie-4',
@@ -73,7 +74,8 @@ export const movieList: MovieItem[] = [
     url: '',
     rating: 7.5,
     runtime: 160,
-    overview: '',
+    overview:
+      "Cobb and Arthur are \"extractors\"; they perform corporate espionage using experimental dream-sharing technology to infiltrate their targets' subconscious and extract information. Their latest target, Saito, is impressed with Cobb's ability to layer multiple dreams within each other and offers to hire Cobb for the supposedly impossible job of implanting an idea into a person's subconscious; performing \"inception\" on Robert, the son of Saito's competitor Maurice Fischer, with the idea to dissolve his father's company. Saito promises to clear Cobb's criminal status, allowing him to return home to his children. ",
   },
   {
     id: 'test-movie-5',
@@ -84,6 +86,7 @@ export const movieList: MovieItem[] = [
     url: '',
     rating: 6.9,
     runtime: 130,
-    overview: '',
+    overview:
+      'When a simple jewelry heist goes horribly wrong, the surviving criminals begin to suspect that one of them is a police informant',
   },
 ];
