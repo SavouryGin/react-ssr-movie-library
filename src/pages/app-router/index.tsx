89ef9e5ceb4d0b3b1d497ccb528cec6dc596b0ec
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-07-08 12:30:33 +0200

    task-8: implement AC1 & AC2

--- src/pages/app-router/index.tsx
+++ src/pages/app-router/index.tsx
@@ -2,14 +2,15 @@ import Home from 'pages/home';
 import Layout from 'components/layout';
 import NotFoundPage from 'pages/not-found';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function AppRouter(): React.ReactElement {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route path='/search' element={<Home />} />
+          <Route path='/' element={<Navigate to='/search' replace />} />
           <Route path='*' element={<NotFoundPage />} />
         </Route>
       </Routes>
