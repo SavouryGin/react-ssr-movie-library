505ff95303927f4887fa3edcbc3027e0e0ca907c
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-07-24 12:22:47 +0200

    task-10: configure global types

---
+++ @types/app/index.d.ts
@@ -0,0 +1,28 @@
+import { ServerResponse } from 'webpack-dev-middleware';
+import 'express';
+
+declare global {
+  interface Window {
+    IS_SERVER: boolean;
+    IS_DEV: boolean;
+  }
+}
+
+declare module 'express' {
+  export interface Response {
+    locals: {
+      nonce: string;
+      webpack: {
+        devMiddleware: import('webpack-dev-middleware').Context<import('http').IncomingMessage, ServerResponse>;
+      };
+    };
+
+    renderApp(): void;
+  }
+}
+
+declare module 'express-serve-static-core' {
+  export interface Response {
+    renderApp(): void;
+  }
+}
