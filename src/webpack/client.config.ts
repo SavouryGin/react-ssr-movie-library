f7775fee7f114bed95776911e3855f9aacf58000
Dmitrii Suroviagin <dmitrii_suroviagin@epam.com>
2022-07-24 11:53:53 +0200

    task-10: add new webpack configs

---
+++ src/webpack/client.config.ts
@@ -0,0 +1,40 @@
+import * as plugins from '../plugins';
+import * as rules from '../rules';
+import { DIST_DIR, IS_DEV, ROOT_DIR } from '../env';
+import { devServerUrl } from '../utils/devServer';
+import type { Configuration } from 'webpack';
+
+const config: Configuration = {
+  name: 'client',
+  target: 'web',
+  devtool: IS_DEV ? 'eval-cheap-module-source-map' : 'source-map',
+  context: ROOT_DIR,
+  mode: IS_DEV ? 'development' : 'production',
+  entry: [IS_DEV && 'webpack-hot-middleware/client?name=client', './src/client'].filter(Boolean),
+  output: {
+    path: DIST_DIR,
+    filename: IS_DEV ? '[name].client.js' : '[name].[contenthash].client.js',
+    publicPath: IS_DEV ? devServerUrl : '/',
+  },
+  module: {
+    rules: [
+      rules.javascriptRule,
+      rules.typescriptRule,
+      rules.htmlRule,
+      rules.mediasRule,
+      rules.fontsRule,
+      rules.cssRule,
+      ...rules.svgRules,
+    ],
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx', '.scss'],
+    plugins: [plugins.tsPaths],
+  },
+  plugins: [plugins.loadablePlugin, plugins.miniCssExtractPlugin, plugins.refreshPlugin, plugins.hmr, plugins.definePlugin()].filter(
+    Boolean,
+  ),
+};
+
+export default config;
---
+++ src/webpack/spa.config.ts
@@ -0,0 +1,44 @@
+import * as plugins from '../plugins';
+import * as rules from '../rules';
+import { DIST_DIR, ROOT_DIR } from '../env';
+import { devServerConfig } from '../utils/devServer';
+
+const config = {
+  name: 'spa',
+  target: 'web',
+  devtool: 'eval-cheap-module-source-map',
+  mode: 'development',
+  context: ROOT_DIR,
+  entry: './src/client',
+  output: {
+    path: DIST_DIR,
+    publicPath: '/',
+    filename: 'js/[name].[fullhash].js',
+  },
+  module: {
+    rules: [
+      rules.javascriptRule,
+      rules.typescriptRule,
+      rules.htmlRule,
+      rules.mediasRule,
+      rules.fontsRule,
+      rules.cssRule,
+      ...rules.svgRules,
+    ],
+  },
+  devServer: devServerConfig,
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json', '.ts', '.tsx', '.scss'],
+    plugins: [plugins.tsPaths],
+  },
+  plugins: [
+    plugins.loadablePlugin,
+    plugins.miniCssExtractPlugin,
+    plugins.refreshPlugin,
+    plugins.hmr,
+    plugins.htmlWebpackPlugin,
+    plugins.definePlugin({ spa: true }),
+  ].filter(Boolean),
+};
+
+export default config;
